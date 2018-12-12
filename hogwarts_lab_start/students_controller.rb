8806bb8fd3f802255ccd45785e058d2e461ef7c9
Rick Symington <rick.symington@gmail.com>
2018-12-12 19:19:38 +0000

    MVP complete

---
+++ hogwarts_lab_start/students_controller.rb
@@ -0,0 +1,45 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/student')
+require_relative('models/house')
+also_reload('./models/*')
+
+# index
+get '/hogwarts/students' do
+  @students = Student.all()
+  erb(:index)
+end
+# new
+get '/hogwarts/students/new' do  # new
+  erb( :new )
+end
+
+# show
+get '/hogwarts/students/:id' do
+  @student = Student.find( params[:id] )
+  erb( :show )
+end
+
+# create
+post '/hogwarts/students' do # create
+  @student = Student.new( params )
+  @student.save()
+  erb( :create )
+end
+# edit
+get '/hogwarts/students/:id/edit' do # edit
+  @student = Student.find( params[:id] )
+  erb( :edit )
+end
+# update
+post '/hogwarts/students/:id' do # update
+  Student.new( params ).update
+  redirect to '/hogwarts/students'
+end
+
+# destroy
+post '/hogwarts/students/:id/delete' do # delete
+  student = Student.find( params[:id] )
+  student.delete()
+  redirect to '/hogwarts/students'
+end
